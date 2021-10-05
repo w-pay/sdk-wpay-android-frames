b97c8dda127a8db9ce86f80e37dec1118f321bdb
Kieran Simpson <kierans777@gmail.com>
2021-10-05 12:43:34 +1100

    Extracted UI into fragment for extension

---
+++ app/src/main/java/au/com/wpay/frames/sample/FramesHost.kt
@@ -0,0 +1,129 @@
+package au.com.wpay.frames.sample
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import au.com.wpay.frames.*
+import au.com.wpay.frames.dto.CardCaptureResponse
+import au.com.wpay.frames.types.ActionType
+import au.com.wpay.frames.types.ControlType
+import au.com.wpay.frames.types.FramesConfig
+import au.com.wpay.frames.types.LogLevel
+
+class FramesHost : Fragment(R.layout.frames_host), FramesView.Callback {
+    companion object {
+        val HTML_KEY = "HTML"
+    }
+
+    private lateinit var messageView: TextView
+    private lateinit var framesView: FramesView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        messageView = view.findViewById(R.id.messageText)
+        framesView = view.findViewById(R.id.framesView)
+
+        /*
+         * Step 1.
+         *
+         * We need to configure the SDK to provide the "bridge" between the native and web worlds.
+         */
+        framesView.configure(
+            config = FramesView.FramesViewConfig(
+                /*
+                 * Note: The SDK will add the required <script> tag to inject the JS SDK into the
+                 * host page. Applications can however add other web content to the page to aid
+                 * in styling.
+                 *
+                 * Note: The SDK will inject a default <meta> tag setting the viewport if no <meta>
+                 * tag for the viewport is provided in the host HTML.
+                 */
+                html = requireArguments().getString(HTML_KEY) ?: run { throw IllegalStateException("Need host HTML") }
+            ),
+            callback = this,
+            logger = DebugLogger()
+        )
+
+        view.findViewById<Button>(R.id.loadBtn).setOnClickListener {
+            /*
+             * Step 2.
+             *
+             * Load the Frames SDK into the HTML page.
+             */
+            framesView.loadFrames(
+                FramesConfig(
+                    apiKey = "95udD3oX82JScUQ1qyACSOMysyAl93Gb",
+                    authToken = "Bearer jbst7UCKR695D93j8tfAd5fG7k2m",
+                    apiBase = "https://dev.mobile-api.woolworths.com.au/wow/v1/pay/instore",
+                    logLevel = LogLevel.DEBUG
+                )
+            )
+        }
+
+        view.findViewById<Button>(R.id.submitBtn).setOnClickListener { SubmitFormCommand.post(framesView) }
+        view.findViewById<Button>(R.id.clearBtn).setOnClickListener {
+            ClearFormCommand.post(framesView)
+            messageView.text = ""
+        }
+    }
+
+    override fun onComplete(response: String) {
+        debug("onComplete(response: $response)")
+
+        val data = CardCaptureResponse.fromJson(response)
+        val message = "${data.status?.responseText} - ${data.paymentInstrument?.itemId}"
+
+        messageView.text = message
+    }
+
+    override fun onError(error: FramesError) {
+        debug("onError(error: $error)")
+
+        messageView.text = error.errorMessage.toString()
+    }
+
+    override fun onProgressChanged(progress: Int) {
+        debug("onProgressChanged(progress: $progress)")
+    }
+
+    override fun onValidationChange(domId: String, isValid: Boolean) {
+        debug("onValidationChange($domId, isValid: $isValid)")
+    }
+
+    override fun onFocusChange(domId: String, isFocussed: Boolean) {
+        debug("onFocusChange($domId, isFocussed: $isFocussed)")
+    }
+
+    override fun onPageLoaded() {
+        debug("onPageLoaded()")
+
+        val captureOptions = ActionType.CaptureCard.Payload(
+            verify = true,
+            save = true,
+            env3DS = null
+        )
+
+        /*
+         * Step 3.
+         *
+         * Add a single line card group to the page
+         */
+        BuildFramesCommand(
+            ActionType.CaptureCard(captureOptions).toCommand(),
+            StartActionCommand,
+            CreateActionControlCommand(ControlType.CARD_GROUP,"cardElement")
+        ).post(framesView)
+    }
+
+    override fun onRendered() {
+        debug("onRendered()")
+    }
+}
+
+fun debug(message: String) {
+    Log.d("[Callback]", message)
+}
--- app/src/main/java/au/com/wpay/frames/sample/MainActivity.kt
+++ app/src/main/java/au/com/wpay/frames/sample/MainActivity.kt
@@ -1,126 +1,24 @@
 package au.com.wpay.frames.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.TextView
-import au.com.wpay.frames.*
-import au.com.wpay.frames.dto.CardCaptureResponse
-import au.com.wpay.frames.types.ActionType
-import au.com.wpay.frames.types.ControlType
-import au.com.wpay.frames.types.FramesConfig
-import au.com.wpay.frames.types.LogLevel
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
-class MainActivity : AppCompatActivity(), FramesView.Callback {
-    private lateinit var messageView: TextView
-    private lateinit var framesView: FramesView
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
-        messageView = findViewById(R.id.messageText)
-        framesView = findViewById(R.id.framesView)
+        savedInstanceState ?: run {
+            val args = bundleOf(FramesHost.HTML_KEY to "<html><body><div id='cardElement'></div></body></html>")
 
-        /*
-         * Step 1.
-         *
-         * We need to configure the SDK to provide the "bridge" between the native and web worlds.
-         */
-        framesView.configure(
-            config = FramesView.FramesViewConfig(
-                /*
-                 * Note: The SDK will add the required <script> tag to inject the JS SDK into the
-                 * host page. Applications can however add other web content to the page to aid
-                 * in styling.
-                 *
-                 * Note: The SDK will inject a default <meta> tag setting the viewport if no <meta>
-                 * tag for the viewport is provided in the host HTML.
-                 */
-                html = "<html><body><div id='cardElement'></div></body></html>"
-            ),
-            callback = this,
-            logger = DebugLogger()
-        )
-
-        findViewById<Button>(R.id.loadBtn).setOnClickListener {
-            /*
-             * Step 2.
-             *
-             * Load the Frames SDK into the HTML page.
-             */
-            framesView.loadFrames(
-                FramesConfig(
-                    apiKey = "95udD3oX82JScUQ1qyACSOMysyAl93Gb",
-                    authToken = "Bearer jbst7UCKR695D93j8tfAd5fG7k2m",
-                    apiBase = "https://dev.mobile-api.woolworths.com.au/wow/v1/pay/instore",
-                    logLevel = LogLevel.DEBUG
-                )
-            )
-        }
-
-        findViewById<Button>(R.id.submitBtn).setOnClickListener { SubmitFormCommand.post(framesView) }
-        findViewById<Button>(R.id.clearBtn).setOnClickListener {
-          ClearFormCommand.post(framesView)
-          messageView.text = ""
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<FramesHost>(R.id.framesHostContainer, args = args)
+            }
         }
     }
-
-    override fun onComplete(response: String) {
-        debug("onComplete(response: $response)")
-
-        val data = CardCaptureResponse.fromJson(response)
-        val message = "${data.status?.responseText} - ${data.paymentInstrument?.itemId}"
-
-        messageView.text = message
-    }
-
-    override fun onError(error: FramesError) {
-        debug("onError(error: $error)")
-
-        messageView.text = error.errorMessage.toString()
-    }
-
-    override fun onProgressChanged(progress: Int) {
-        debug("onProgressChanged(progress: $progress)")
-    }
-
-    override fun onValidationChange(domId: String, isValid: Boolean) {
-        debug("onValidationChange($domId, isValid: $isValid)")
-    }
-
-    override fun onFocusChange(domId: String, isFocussed: Boolean) {
-        debug("onFocusChange($domId, isFocussed: $isFocussed)")
-    }
-
-    override fun onPageLoaded() {
-        debug("onPageLoaded()")
-
-        val captureOptions = ActionType.CaptureCard.Payload(
-            verify = true,
-            save = true,
-            env3DS = null
-        )
-
-        /*
-         * Step 3.
-         *
-         * Add a single line card group to the page
-         */
-        BuildFramesCommand(
-            ActionType.CaptureCard(captureOptions).toCommand(),
-            StartActionCommand,
-            CreateActionControlCommand(ControlType.CARD_GROUP,"cardElement")
-        ).post(framesView)
-    }
-
-    override fun onRendered() {
-        debug("onRendered()")
-    }
-}
-
-fun debug(message: String) {
-    Log.d("[Callback]", message)
 }
