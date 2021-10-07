1bd66f62a6faa36c6b2991078aa9c9422407bb22
Kieran Simpson <kierans777@gmail.com>
2021-10-07 17:07:00 +1100

    Simplify usage of DrawerLayout.

--- app/src/main/java/au/com/wpay/frames/sample/MainActivity.kt
+++ app/src/main/java/au/com/wpay/frames/sample/MainActivity.kt
@@ -2,54 +2,37 @@ package au.com.wpay.frames.sample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer)
         val navController = findNavController()
         val navView = findViewById<NavigationView>(R.id.nav_view)
 
-        drawerLayout = findViewById(R.id.drawer)
         appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
 
         setupActionBarWithNavController(navController, appBarConfiguration)
-
-        navView.setNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.singleCardCaptureItem -> navController.navigate(R.id.singleCardCapture)
-                R.id.multiLineCardCaptureItem -> navController.navigate(R.id.multiLineCardCapture)
-                else -> throw IllegalStateException("Unknown menu item id")
-            }
-
-            drawerLayout.close()
-
-            true
-        }
+        navView.setupWithNavController(navController)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return when {
-            drawerLayout.isOpen -> {
-                drawerLayout.close()
-
-                true
-            }
-            else -> {
-                findNavController().navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-            }
-        }
-    }
+    override fun onSupportNavigateUp(): Boolean =
+        findNavController().navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
 
     private fun findNavController(): NavController {
         val host = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
