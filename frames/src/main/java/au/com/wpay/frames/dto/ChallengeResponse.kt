f298bbe05816fd730d8563b21cf169fbc61036df
Kieran Simpson <kierans777@gmail.com>
2021-11-10 13:20:16 +1100

    Add response model for validating a card.

--- frames/src/main/java/au/com/wpay/frames/dto/ChallengeResponse.kt
+++ frames/src/main/java/au/com/wpay/frames/dto/ChallengeResponse.kt
@@ -4,6 +4,7 @@ import org.json.JSONObject
 import java.io.Serializable
 
 data class ChallengeResponse(
+    val instrumentId: String?,
     val reference: String?,
     val token: String?,
     val type: String?
@@ -14,9 +15,19 @@ data class ChallengeResponse(
 
         fun fromJson(json: JSONObject): ChallengeResponse =
             ChallengeResponse(
+                json.optString("instrumentId"),
                 json.optString("reference"),
                 json.optString("token"),
                 json.optString("type")
             )
+
     }
+
+    fun toJson(): JSONObject =
+        JSONObject().apply {
+            instrumentId?.let { put("instrumentId", it) }
+            reference?.let { put("reference", it) }
+            token?.let { put("token", it) }
+            type?.let { put("type", it) }
+        }
 }
